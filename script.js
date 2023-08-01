8fd44eed62bd69a42fdc7080bca06116a98a656d
dailycontest <96362790+lfgraphics@users.noreply.github.com>
2023-08-01 20:55:47 +0530

    game status update

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const cells = document.querySelectorAll('.box');
 let cellArr = [...cells];
+
 let currentPlayer = "X";
+let playerIndicator = document.getElementById('player');
+
 let gameStatus = "inProgress";
+
+let XWinStatus = document.getElementById('XW');
+let OWinStatus = document.getElementById('OW');
+let DrawStatus = document.getElementById('Draw');
+
+playerIndicator.textContent = currentPlayer
 
 const trackCells = () => ({
     1: cells[0].innerHTML,
@@ -21,12 +30,13 @@ const winningCombinations = [
     [0, 4, 8], [2, 4, 6],
 ];
 
-const board = ['', '', '', '', '', '', '', '', ''];
+let board = ['', '', '', '', '', '', '', '', ''];
 
 
 
 function switchPlayer() {
     currentPlayer = currentPlayer === "X" ? "O" : "X";
+    playerIndicator.textContent = currentPlayer
 }
 
 function checkWin() {
@@ -38,6 +48,17 @@ function checkWin() {
             cellArr[a].classList.add("winning-cell");
             cellArr[b].classList.add("winning-cell");
             cellArr[c].classList.add("winning-cell");
+
+            console.log(gameStatus)
+
+            if(gameStatus == 'X Won'){
+                XWinStatus.innerHTML = parseInt(XWinStatus.innerHTML) + 1;
+            } else if (gameStatus == 'O Won'){
+                OWinStatus.innerHTML = parseInt(OWinStatus.innerHTML) + 1;
+            } else if(gameStatus == 'Draw'){
+                DrawStatus.innerHTML = parseInt(DrawStatus.innerHTML) + 1;
+            }
+
             setTimeout(() => {
                 alert(gameStatus);
                 endGame();
@@ -51,6 +72,7 @@ function checkWin() {
 
 
 function checkDraw() {
+    gameStatus = 'Draw'
     return board.every(cell => cell !== '');
 }
 
@@ -66,7 +88,7 @@ function endGame() {
 
 
 function makeMove(cellIndex) {
-    if (board[cellIndex] === '') {
+    if (board[cellIndex] === '' && gameStatus !== 'X Won' && gameStatus !== 'O Won') {
         board[cellIndex] = currentPlayer;
         cellArr[cellIndex].innerHTML = currentPlayer;
 
@@ -75,9 +97,7 @@ function makeMove(cellIndex) {
                 endGame();
             }, 3000)
         } else if (checkDraw()) {
-
             endGame();
-
         } else {
             switchPlayer();
         }
@@ -92,5 +112,5 @@ cellArr.forEach((cell, index) => {
 
 
 let XW = document.getElementById('XW')
-XW.innerHTML = parseInt(XW.innerHTML) + 1
+// XW.innerHTML = parseInt(XW.innerHTML) + 1
 // Number(document.getElementById('XW').textContent) +1
